c7cc5e20f581a40d112ad3dd7c0e0d1526470388
Omar Bouraoui <93351026+omarbouraoui01@users.noreply.github.com>
2023-12-12 22:32:17 +0100

    Version finale

--- Omar/EcoGive/View/RecDisplay.swift
+++ Omar/EcoGive/View/RecDisplay.swift
@@ -30,10 +30,10 @@ struct NewsDisp: View {
         }
         .padding()
         .background(
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.white)
-                .shadow(radius: 5)
-        )
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 0xAFC8AD)) // Background color AFC8AD
+                        .shadow(radius: 5)
+                )
     }
 }
 struct RecDisplay: View {
@@ -41,35 +41,134 @@ struct RecDisplay: View {
     let description: String
     let type: String
     let userName: String
+    @State private var isReclamationDetailVisible: Bool = false
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-               Text(title)
-                   .font(.title)
-                   .fontWeight(.bold)
-               Text(description)
-                   .foregroundColor(.secondary)
-               HStack {
-                   Text("Type:")
-                       .foregroundColor(.secondary)
-                   Text(type)
-               }
-               HStack {
-                   Text("User:")
-                       .foregroundColor(.secondary)
-                   Text(userName)
-               }
-           }
-           .padding()
-           .frame(maxWidth: .infinity) // Ensure the content takes the full width
-           .background(
-               RoundedRectangle(cornerRadius: 10)
-                   .fill(Color.white)
-                   .shadow(radius: 5)
-           )
-           .padding(.horizontal, 10) // Optional: Add padding to the sides of the VStack
-       }
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+            Text(description)
+                .foregroundColor(.secondary)
+            HStack {
+                Text("Type:")
+                    .foregroundColor(.secondary)
+                Text(type)
+            }
+            HStack {
+                Text("User:")
+                    .foregroundColor(.secondary)
+                Text(userName)
+            }
+
+            // Button to show detailed reclamation
+            Button(action: {
+                // Here you can handle the logic to show the detailed reclamation
+                // and update the UI to display the admin response.
+                isReclamationDetailVisible.toggle()
+            }) {
+                Text("View Response")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color(hex: 0x5F6F52))
+                    .cornerRadius(5)
+            }
+            .sheet(isPresented: $isReclamationDetailVisible) {
+                // DetailedReclamationView is a separate view for the detailed reclamation.
+                DetailedReclamationView(adminResponse: "Your reclamation has been responded")
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 0xAFC8AD)) // Background color AFC8AD
+                        .shadow(radius: 5)
+                )
+        .padding(.horizontal, 10)
     }
+}
+
+// Separate view for detailed reclamation
+
+
+
+
+
+struct DetailedReclamationView: View {
+    let adminResponse: String
+    @State private var selectedStarCount: Int = 0
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        
+        VStack {
+            
+            // Admin Avatar
+            Image("omar") // Replace "adminAvatar" with the actual name of your admin avatar image
+                .resizable()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 5)
+
+            // Admin Response
+            Text(adminResponse)
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            // Star Rating
+            HStack {
+                ForEach(1...5, id: \.self) { index in
+                    Image(systemName: index <= selectedStarCount ? "star.fill" : "star")
+                        .foregroundColor(.yellow)
+                        .onTapGesture {
+                            // Update the selected star count when tapped
+                            selectedStarCount = index
+                        }
+                }
+            }
+            .padding()
+
+            // Back Button
+            Button(action: {
+                // Dismiss the current view (navigate back to RecDisplay)
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                HStack {
+                    Image(systemName: "chevron.left")
+                    Text("Back")
+                        .fontWeight(.semibold)
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color(hex: 0x5F6F52))
+                .cornerRadius(8)
+            }
+            .padding()
+
+            Spacer()
+        }
+        .padding()
+        .background(Color(hex: 0xAFC8AD))
+        .cornerRadius(10)
+        .shadow(radius: 5)
+        .padding()
+    }
+}
+
+
+
+
+struct DetailedReclamationView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedReclamationView(adminResponse: "Your reclamation has been responded. See you soon !!")
+    }
+}
+
+
 
 struct HomeView: View {
     @State private var reclamations: [ReclamationData] = []
@@ -113,31 +212,31 @@ struct HomeView: View {
         }
     }
 }
+
 struct NewsDisplay: View {
     @State private var news: [NewsData] = []
-
+    
     var body: some View {
         NavigationView {
-            List(news, id: \.id) { test in
-                NewsDisp(
-                    title: test.title,
-                    description: test.description,
-                    date: test.date
-                    
-                )
+            
+            ZStack{
+             
+                List(news, id: \.id) { test in
+                    NavigationLink(destination: NewsDetail(newsData: test)) {
+                        NewsDisp(
+                            title: test.title,
+                            description: test.description,
+                            date: test.date
+                        )
+                    }
+                }
             }
+            .background(Color(hex: 0xAFC8AD))
             .onAppear {
                 Task {
                     do {
                         let url = URL(string: "http://localhost:3001/api/news/")!
                         let (data, _) = try await URLSession.shared.data(from: url)
-
-                        do {
-                            let json = try JSONSerialization.jsonObject(with: data, options: [])
-                            print("JSON Response: \(json)")
-                        } catch {
-                            print("Error converting data to JSON: \(error)")
-                        }
 
                         let response = try JSONDecoder().decode([String: [NewsData]].self, from: data)
 
@@ -147,7 +246,7 @@ struct NewsDisplay: View {
                             print("Error: 'message' key not found in the response")
                         }
                     } catch {
-                        print("Error fetching reclamations: \(error)")
+                        print("Error fetching news: \(error)")
                     }
                 }
             }
@@ -155,6 +254,72 @@ struct NewsDisplay: View {
         }
     }
 }
+
+
+
+struct NewsDetail: View {
+    let newsData: NewsData
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                // Static Image
+                Image("ghaza") // Replace "your_static_image_name" with the actual name of your image asset
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .cornerRadius(12)
+                    .padding(.horizontal, 16)
+
+                // News Details
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(newsData.title)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text(newsData.description)
+                        .font(.body)
+                        .foregroundColor(.secondary)
+                    Text("Date: \(newsData.date)")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                .padding(.horizontal, 16)
+
+                // Share Button
+                Button(action: {
+                    // Create a URL to share, for example, the news article link
+                    let newsURL = URL(string: "https://ecocive.tn/news/\(newsData.id)")!
+
+                    // Create an instance of UIActivityViewController
+                    let activityViewController = UIActivityViewController(activityItems: [newsURL], applicationActivities: nil)
+
+                    // Exclude some activities from the share sheet if needed
+                    activityViewController.excludedActivityTypes = [
+                        .addToReadingList,
+                        .airDrop,
+                        .assignToContact
+                    ]
+
+                    // Present the UIActivityViewController
+                    UIApplication.shared.windows.first?.rootViewController?.present(activityViewController, animated: true, completion: nil)
+                }) {
+                    Text("Share")
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+            }
+        }
+        .navigationBarTitle("News Detail")
+    }
+}
+
+
 
 
 struct AddReclamationView: View {
@@ -168,36 +333,41 @@ struct AddReclamationView: View {
 
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("Reclamation Details")) {
-                    TextField("Title", text: $title)
-                    TextField("Description", text: $description)
-                    TextField("Type", text: $type)
-                    TextField("User", text: $userName)
-                }
-
-                Section {
-                    Button("Save Reclamation", action: {
-                        Task {
-                            await addReclamation()
+            ZStack{
+              
+                VStack{
+                    Form {
+                        Section(header: Text("Reclamation Details")) {
+                            TextField("Title", text: $title)
+                            TextField("Description", text: $description)
+                            TextField("Type", text: $type)
+                            TextField("User", text: $userName)
                         }
-                    })
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(8)
+                        
+                        Section {
+                            Button("Save Reclamation", action: {
+                                Task {
+                                    await addReclamation()
+                                }
+                            })
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(hex: 0x5F6F52))
+                            .cornerRadius(8)
+                        }
+                    }
+                    .navigationBarTitle("Add Reclamation")
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Success"),
+                            message: Text("Reclamation added successfully!"),
+                            dismissButton: .default(Text("OK"), action: {
+                                navigateToHome = true
+                            })
+                        )
+                    }
                 }
-            }
-            .navigationBarTitle("Add Reclamation")
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("Success"),
-                    message: Text("Reclamation added successfully!"),
-                    dismissButton: .default(Text("OK"), action: {
-                        navigateToHome = true
-                    })
-                )
             }
             .background(
                 NavigationLink(
@@ -206,6 +376,8 @@ struct AddReclamationView: View {
                     label: { EmptyView() }
                 )
             )
+            .background(Color(hex: 0xAFC8AD))
+            
         }
     }
     
@@ -275,6 +447,7 @@ struct NewsData: Codable, Identifiable {
 
 struct ContentView: View {
     var body: some View {
+        
         TabView {
             HomeView()
                 .tabItem {
@@ -284,21 +457,40 @@ struct ContentView: View {
 
             AddReclamationView()
                 .tabItem {
+                    
                     Image(systemName: "plus.circle.fill")
                     Text("Add Reclamation")
                 }
+
             NewsDisplay()
                 .tabItem {
                     Image(systemName: "square.fill")
                     Text("News")
                 }
         }
+        .background(
+            Image("Back") // Replace "backgroundImage" with the actual name of your background image
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+        )
     }
 }
-
+extension Color {
+    init(hex: UInt) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 8) & 0xff) / 255,
+            blue: Double(hex & 0xff) / 255,
+            opacity: 1
+        )
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
 
